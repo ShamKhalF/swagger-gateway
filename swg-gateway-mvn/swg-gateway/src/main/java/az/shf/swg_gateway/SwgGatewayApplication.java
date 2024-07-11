2056eb9bf4bed12cd95c4b190024378547d5b51e
ShamKhalF <104983627+ShamKhalF@users.noreply.github.com>
2024-07-11 10:52:13 +0400

    nem vallah

--- swg-gateway-mvn/swg-gateway/src/main/java/az/shf/swg_gateway/SwgGatewayApplication.java
+++ swg-gateway-mvn/swg-gateway/src/main/java/az/shf/swg_gateway/SwgGatewayApplication.java
@@ -1,7 +1,9 @@
 package az.shf.swg_gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
--- swg-gateway-mvn/swg-gateway/src/test/java/az/shf/swg_gateway/SwgGatewayApplicationTests.java
+++ swg-gateway-mvn/swg-gateway/src/test/java/az/shf/swg_gateway/SwgGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package az.shf.swg_gateway;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SwgGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package az.shf.swg_gateway;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SwgGatewayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
